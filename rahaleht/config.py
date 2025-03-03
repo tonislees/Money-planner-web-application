66cc461d253c6d8f2b0ae803d46512a9712807ec
tonislees <tonis.lees@gmail.com>
2025-03-03 15:38:21 +0200

    Update config.py

--- rahaleht/config.py
+++ rahaleht/config.py
@@ -2,12 +2,12 @@ import os
 
 
 class Config:
-    SECRET_KEY = 'xXK2A@7!Rq&PUjZ&$*k4G9'
+    SECRET_KEY = 'xXK2A@7!Rq&PUjZ&$*k4G9' // insert here your own private key, this should be private
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "site.db")}'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = "rahaleht@gmail.com"
-    MAIL_PASSWORD = "knvicrdgwtjqhdup"
+    MAIL_USERNAME = "youremail@gmail.com" // insert your email here, the email sends password reset emails
+    MAIL_PASSWORD = "abcdefghijklmnopqr" // insert your email password here
